6f9acc588671d07645ca036299c271815c97f7ce
Maxim Elizmarov <kiro.dota322@gmail.com>
2022-11-22 15:37:50 +0300

    addDZ

---
+++ dz_1_zadanie/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine()); // ввод чисел
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine()); // ввод чисел
+Console.Write("Большее число: ");
+
+if (a > b)
+    Console.Write(a);    
+else
+    Console.Write(b);    
+
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
---
+++ dz_2_zadanie/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine()); // ввод чисел
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine()); // ввод чисел
+Console.Write("Введите 3-ое число: ");
+int c = Convert.ToInt32(Console.ReadLine()); // ввод чисел
+Console.Write("Большее число: ");
+if (a >= b & a >= c)
+    Console.Write(a);
+else if (b >= a & b >= c )
+    Console.Write(b);
+else if (c >= a & c >= b)
+    Console.Write(c);
+
+// Задача 4: Напишите программу, которая принимает на входдаёт максимальное из три числа и вы этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
